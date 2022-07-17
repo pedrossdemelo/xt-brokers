cb1540f4f3f9d2113f31b6464578988aad8292a2
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-17 14:25:49 -0300

    refactor: typings

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -7,14 +7,17 @@ function writeInitialState<T>(key: string, value: T) {
 }
 
 export default function useLocalStorage<T>(key: string, initialState: T) {
-  const [storedValue, setStoredValue] = useState(
+  const [storedValue, setStoredValue] = useState<T>(
     JSON.parse(localStorage.getItem(key)!) ??
       writeInitialState(key, initialState)
   );
 
-  function setValue(value: T) {
-    localStorage.setItem(key, JSON.stringify(value));
+  const setValue: typeof setStoredValue = (value) => {
     setStoredValue(value);
+    localStorage.setItem(
+      key,
+      JSON.stringify(typeof value === "function" ? (value as Function)(storedValue) : value)
+    );
   }
 
   return [storedValue as T, setValue] as const;
