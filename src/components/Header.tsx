1823ab53f94acfe49e9c1d9f79354bda257d0e89
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-17 14:26:07 -0300

    feat(components): add funds link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,30 @@
+import { Button } from "flowbite-react";
 import useUserData from "hooks/useUserData";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
-export default function Header({}: Props) {
+function useHeader() {
   const { user, loggedAt } = useUserData();
+  const navigate = useNavigate();
+
+  function goToFunds() {
+    navigate("/funds");
+  }
+
+  return { user, loggedAt, goToFunds };
+}
+
+export default function Header({}: Props) {
+  const { user, loggedAt, goToFunds } = useHeader();
 
   return (
     <>
       <div>Header</div>
       <div id="user">User: {user}</div>
       <div id="last-login">loggedAt: {loggedAt.toISOString()}</div>
+      <Button onClick={goToFunds}>Funds</Button>
     </>
   );
 }
