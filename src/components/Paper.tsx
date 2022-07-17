0ae987bfacbf3291488aff7c7e944996b36fb121
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-17 14:25:18 -0300

    feat(components): implement Paper component logic

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -1,17 +1,83 @@
+import { Button } from "flowbite-react";
+import usePapers from "hooks/usePapers";
+import useUserData from "hooks/useUserData";
+import produce from "immer";
 import React from "react";
+import { papers } from "utils/mockData";
+
+type InferArray<T> = T extends (infer U)[] ? U : never;
+type Paper = InferArray<typeof papers>;
 
 type Props = {
-  amount: number;
-  ticker: string;
-  price: number;
+  data: Paper;
+  personal?: boolean;
 };
 
-export default function Paper({ amount, ticker, price }: Props) {
+function usePaper(paper: Paper) {
+  const { setAllPapers } = usePapers();
+  const { setUserPapers } = useUserData();
+
+  function sellPaper(amount: number) {
+    setUserPapers(
+      produce((draft: Paper[]) => {
+        const index = draft.findIndex(p => p.CodAtivo === paper.CodAtivo);
+        if (index !== -1 && draft[index].QteAtivo >= amount) {
+          draft[index].QteAtivo -= amount;
+        } else {
+          alert("error");
+        }
+      })
+    );
+
+    setAllPapers(
+      produce((draft: Paper[]) => {
+        const index = draft.findIndex(p => p.CodAtivo === paper.CodAtivo);
+        if (index !== -1) {
+          draft[index].QteAtivo += amount;
+        } else {
+          draft.push({ ...paper, QteAtivo: amount });
+        }
+      })
+    );
+  }
+
+  function buyPaper(amount: number) {
+    setUserPapers(
+      produce(draft => {
+        const index = draft.findIndex(p => p.CodAtivo === paper.CodAtivo);
+        if (index !== -1) {
+          draft[index].QteAtivo += amount;
+        } else {
+          draft.push({ ...paper, QteAtivo: amount });
+        }
+      })
+    );
+
+    setAllPapers(
+      produce(draft => {
+        const index = draft.findIndex(p => p.CodAtivo === paper.CodAtivo);
+        if (index !== -1 && draft[index].QteAtivo >= amount) {
+          draft[index].QteAtivo -= amount;
+        } else {
+          alert("error");
+        }
+      })
+    );
+  }
+
+  return { sellPaper, buyPaper };
+}
+
+export default function Paper({ data, personal }: Props) {
+  const { sellPaper, buyPaper } = usePaper(data);
+
   return (
-    <>
-      <div>{amount}</div>
-      <div>{ticker}</div>
-      <div>{price}</div>
-    </>
+    <div>
+      <div>
+        {data.CodAtivo} {data.Valor} {data.QteAtivo}{" "}
+      </div>
+      <Button onClick={() => buyPaper(1)}>Buy</Button>
+      {personal && <Button onClick={() => sellPaper(1)}>Sell</Button>}
+    </div>
   );
 }
