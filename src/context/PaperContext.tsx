b58a7c569d6bed64b205993fb1a073d46180f73a
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-16 21:29:32 -0300

    feat(context): separate PaperContext from UserContext

---
+++ src/context/PaperContext.tsx
@@ -0,0 +1,34 @@
+import useLocalStorage from 'hooks/useLocalStorage';
+import React from 'react'
+import { papers } from 'utils/mockData';
+
+type Props = {
+  children: React.ReactNode;
+}
+
+type PaperContextValue = {
+  allPapers: typeof papers;
+  setAllPapers: (paper: typeof papers) => void;
+}
+
+const PaperContext = React.createContext<PaperContextValue>({
+  allPapers: [],
+  setAllPapers: () => {},
+});
+
+export function PaperProvider({ children }: Props) {
+  const [allPapers, setAllPapers] = useLocalStorage('allPapers', papers);
+
+  const value = {
+    allPapers,
+    setAllPapers
+  }
+
+  return (
+    <PaperContext.Provider value={value}>
+      {children}
+    </PaperContext.Provider>
+  )
+}
+
+export default PaperContext;
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,35 +1,33 @@
 import useLocalStorage from "hooks/useLocalStorage";
 import React from "react";
-import { papers } from "utils/MockData";
+import { papers } from "utils/mockData";
 
 type Props = {
   children: React.ReactNode;
 };
 
-type ContextValue = {
-  papers: typeof papers;
-  setPapers: (paper: typeof papers) => void;
+type UserContextValue = {
+  userPapers: typeof papers;
+  setUserPapers: (paper: typeof papers) => void;
   user: string;
   setUser: (user: string) => void;
 };
 
-const UserContext = React.createContext<ContextValue>({
-  papers: [],
-  setPapers: () => {},
+const UserContext = React.createContext<UserContextValue>({
+  userPapers: [],
+  setUserPapers: () => {},
   user: "",
   setUser: () => {},
 });
 
 export function UserProvider({ children }: Props) {
-  const [paperPool, setPapers] = useLocalStorage("paperPool", papers);
+  const [userPapers, setUserPapers] = useLocalStorage(
+    "userPapers",
+    [] as typeof papers
+  );
   const [user, setUser] = useLocalStorage("user", "");
 
-  const value = {
-    papers: paperPool,
-    setPapers,
-    user,
-    setUser,
-  };
+  const value = { userPapers, setUserPapers, user, setUser };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 }
