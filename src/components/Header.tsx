2eabe441fe5670c8e3c46daf938a097779c5fbcb
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-18 15:42:25 -0300

    feat: style Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,132 @@
-import { Button } from "flowbite-react";
+import {
+  CashIcon,
+  CodeIcon,
+  EyeIcon,
+  EyeOffIcon,
+  LogoutIcon,
+  MenuIcon,
+  UserIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import { Dropdown } from "flowbite-react";
 import { useUserData } from "hooks";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function useHeader() {
-  const { user, loggedAt } = useUserData();
+  const { user, funds, logout } = useUserData();
   const navigate = useNavigate();
+  const [showFunds, setShowFunds] = React.useState(true);
 
   function goToFunds() {
     navigate("/funds");
   }
 
-  return { user, loggedAt, goToFunds };
+  function goToDashboard() {
+    navigate("/");
+  }
+
+  return {
+    user,
+    goToFunds,
+    goToDashboard,
+    funds,
+    showFunds,
+    toggleFunds: () => setShowFunds(!showFunds),
+    logout: () => {
+      navigate("/login");
+      logout();
+    },
+  };
 }
 
 export default function Header() {
-  const { user, loggedAt, goToFunds } = useHeader();
+  const {
+    user,
+    goToFunds,
+    goToDashboard,
+    logout,
+    funds,
+    showFunds,
+    toggleFunds,
+  } = useHeader();
 
   if (!user) return null;
 
   return (
-    <>
-      <div>Header</div>
+    <nav className="h-14 flex px-4 py-2 z-50 justify-between items-center bg-slate-900 text-white">
+      <button onClick={toggleFunds}>
+        {showFunds ? (
+          <EyeOffIcon className="h-6" />
+        ) : (
+          <EyeIcon className="h-6" />
+        )}
+      </button>
 
-      <div id="user">User: {user}</div>
+      <h1
+        className={`text-lg font-medium px-2 rounded-md ${
+          !showFunds && "text-slate-800 bg-slate-800"
+        }`}
+      >
+        {funds.toFixed(2)} BRL
+      </h1>
 
-      <div id="last-login">loggedAt: {loggedAt.toISOString()}</div>
+      <Dropdown inline arrowIcon={false} label={<MenuIcon className="h-7" />}>
+        <Dropdown.Item>
+          <div className="flex gap-2 items-center mr-2">
+            <UserIcon className="h-6" />
 
-      <Button onClick={goToFunds}>Funds</Button>
-    </>
+            <span className="ml-2">{user}</span>
+          </div>
+        </Dropdown.Item>
+
+        <Dropdown.Divider />
+
+        <Dropdown.Item>
+          <button
+            onClick={goToDashboard}
+            className="flex gap-2 items-center mr-2"
+          >
+            <ViewGridIcon className="h-6" />
+
+            <span className="ml-2">Dashboard</span>
+          </button>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <a
+            href="https://pedrosousa.dev"
+            target="_blank"
+            className="flex gap-2 items-center mr-2"
+            rel="noreferrer"
+          >
+            <CodeIcon className="h-6" />
+
+            <span className="ml-2">Developer</span>
+          </a>
+        </Dropdown.Item>
+
+        <Dropdown.Item>
+          <button onClick={goToFunds} className="flex gap-2 items-center mr-2">
+            <CashIcon className="h-6" />
+
+            <span className="ml-2">Funds</span>
+          </button>
+        </Dropdown.Item>
+
+        <Dropdown.Divider />
+
+        <Dropdown.Item>
+          <button
+            onClick={logout}
+            className="flex gap-2 items-center mr-2 text-red-700"
+          >
+            <LogoutIcon className="h-6" />
+
+            <span className="ml-2">Logout</span>
+          </button>
+        </Dropdown.Item>
+      </Dropdown>
+    </nav>
   );
 }
