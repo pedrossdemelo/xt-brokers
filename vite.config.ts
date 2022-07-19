9f4dd62ab7f660748bf6578b94a51ff16a672b39
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-19 18:47:19 -0300

    test: add testing libraries
    vitest, jsdom & testing library/react

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
