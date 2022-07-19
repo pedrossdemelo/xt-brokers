ec4ffc988a1a14eab7e0e49d391a02429afd3526
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-19 02:24:16 -0300

    style: tweak color scheme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,18 +94,16 @@ export default function Home() {
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
             focus:ring-blue-300 font-medium rounded-lg px-5 py-3.5 mr-2 mb-2
-            dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none
-            dark:focus:ring-blue-800 w-full"
+            focus:outline-none w-full"
             >
               Login
             </button>
 
             <button
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+              className="text-white bg-emerald-500 hover:bg-emerald-700 focus:ring-4
             focus:ring-blue-300 font-medium rounded-lg px-5 py-3.5 ml-2 mb-2
-            dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none
-            dark:focus:ring-blue-800 w-full"
+            focus:outline-none w-full"
             >
               Sign up
             </button>
