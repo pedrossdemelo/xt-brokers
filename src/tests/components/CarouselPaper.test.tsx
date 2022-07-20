7de160aa66b82eca5636baf2122cf0ce0c4c4328
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 18:20:46 -0300

    test: CarouselPaper

--- src/tests/components/CarouselPaper.test.tsx
+++ src/tests/components/CarouselPaper.test.tsx
@@ -1,10 +1,45 @@
 import { CarouselPaper } from "components";
-import { render } from "tests";
+import { render, screen } from "tests";
 import { papers } from "../mocks";
 
-describe("CarouselPaper component", () => {
-  it.todo("implement tests");
-  it("", () => {
-    render(<CarouselPaper data={papers[0]} />);
+const mockPaper = { ...papers[0], QteAtivo: 10 };
+
+describe("CarouselPaper", () => {
+  beforeEach(() => {
+    render(<CarouselPaper data={mockPaper} />);
+  });
+
+  it("should display the ticker", () => {
+    expect(screen.getByText(mockPaper.CodAtivo)).toBeInTheDocument();
+  });
+
+  it("should display the name", () => {
+    expect(screen.getByText(mockPaper.NomeAtivo)).toBeInTheDocument();
+  });
+
+  it("should display the price", () => {
+    expect(
+      screen.getByText(new RegExp(`${mockPaper.Valor}`, "i")),
+    ).toBeInTheDocument();
+  });
+
+  it("should display the amount in the portfolio", () => {
+    expect(
+      screen.getByText(new RegExp(`${mockPaper.QteAtivo}`, "i")),
+    ).toBeInTheDocument();
+  });
+
+  it("should display the price * amount", () => {
+    expect(
+      screen.getByText(
+        new RegExp((mockPaper.Valor * mockPaper.QteAtivo).toFixed(2), "i"),
+      ),
+    ).toBeInTheDocument();
+  });
+
+  it("should display the variation", () => {
+    expect(
+      screen.getByText(new RegExp(`${mockPaper.Variacao}`, "i")),
+    ).toBeInTheDocument();
   });
 });
