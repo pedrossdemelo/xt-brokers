9066bbd3592f3941756bf97359b34d20c204451b
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 17:05:03 -0300

    test: AllPapers

--- src/tests/components/AllPapers.test.tsx
+++ src/tests/components/AllPapers.test.tsx
@@ -1,11 +1,32 @@
 import { AllPapers } from "components";
-import { render, screen } from "tests";
+import { render, screen, within } from "tests";
+import { papers } from "tests/mocks";
 
-describe("AllPapers component", () => {
-  it.todo("implement tests");
+const mockPapers = papers.slice(0, 5);
 
-  it("", () => {
-    render(<AllPapers />);
+describe("AllPapers", () => {
+  beforeEach(() => {
+    render(<AllPapers />, { mock: { allPapers: mockPapers } });
+  });
+
+  it("should display all papers in the pool", async () => {
+    screen.logTestingPlaygroundURL();
+    for (const paper of mockPapers) {
+      const paperElement = document.getElementById(`trade-${paper.CodAtivo}`)!;
+
+      await within(paperElement).findByText(paper.CodAtivo);
+      await within(paperElement).findByText(paper.NomeAtivo);
+      await within(paperElement).findByText(new RegExp(`${paper.Valor}`, "i"));
+      await within(paperElement).findByText(
+        new RegExp(`${paper.Variacao}`, "i"),
+      );
+      await within(paperElement).findByText(
+        new RegExp(`${paper.QteAtivo}`, "i"),
+      );
+    }
+  });
+
+  it("should display 'stocks' at the top", () => {
     expect(screen.getByText(/stocks/i)).toBeInTheDocument();
   });
 });
