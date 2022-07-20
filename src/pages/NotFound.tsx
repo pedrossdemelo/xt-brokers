6144371bcedf63b3db7895bbdc9b4535d93f01be
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 18:15:33 -0300

    feat: add return to dashboard button to notfound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,30 @@
+import { ViewGridIcon } from "@heroicons/react/solid";
+import { Button } from "flowbite-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function useNotFound() {
+  const navigate = useNavigate();
+
+  const goBack = React.useCallback(() => {
+    navigate("/");
+  }, []);
+
+  return {
+    goBack,
+  };
+}
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const { goBack } = useNotFound();
+
+  return (
+    <div>
+      This page does not exist.
+      <Button color="gray" onClick={goBack}>
+        <ViewGridIcon className="h-6 mr-3" />
+        Return to Dashboard
+      </Button>
+    </div>
+  );
 }
