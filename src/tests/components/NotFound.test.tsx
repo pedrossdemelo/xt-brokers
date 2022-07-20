153545bf11e1a816ce536c75248cb08cf0a0a7f2
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 18:21:01 -0300

    test: NotFound

--- src/tests/components/NotFound.test.tsx
+++ src/tests/components/NotFound.test.tsx
@@ -1,9 +1,18 @@
 import { NotFound } from "pages";
-import { render } from "tests";
+import { render, screen } from "tests";
 
-describe("NotFound component", () => {
-  it.todo("implement tests");
-  it("should render", () => {
+describe("NotFound", () => {
+  beforeEach(() => {
     render(<NotFound />);
+  });
+
+  it("should inform the user that the page doesn't exist", () => {
+    expect(screen.getByText(/not exist/i)).toBeInTheDocument();
+  });
+
+  it("should display a button to return to the dashboard", () => {
+    const button = screen.getByRole("button", { name: /dashboard/i });
+
+    expect(button).toBeInTheDocument();
   });
 });
