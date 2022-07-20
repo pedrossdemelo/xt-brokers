84f7b23f7e32dbd51df54945750d552aab799845
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 18:21:12 -0300

    test: Paper

--- src/tests/components/Paper.test.tsx
+++ src/tests/components/Paper.test.tsx
@@ -1,10 +1,37 @@
 import { Paper } from "components";
-import { render } from "tests";
+import { render, screen } from "tests";
 import { papers } from "../mocks";
 
-describe("Paper component", () => {
-  it.todo("implement tests");
-  it("should render", () => {
-    render(<Paper data={papers[0]} />);
+const mockPaper = { ...papers[0], QteAtivo: 10 };
+
+describe("Paper", () => {
+  beforeEach(() => {
+    render(<Paper data={mockPaper} />);
+  });
+
+  it("should display the ticker", () => {
+    expect(screen.getByText(mockPaper.CodAtivo)).toBeInTheDocument();
+  });
+
+  it("should display the name", () => {
+    expect(screen.getByText(mockPaper.NomeAtivo)).toBeInTheDocument();
+  });
+
+  it("should display the price", () => {
+    expect(
+      screen.getByText(new RegExp(`${mockPaper.Valor}`, "i")),
+    ).toBeInTheDocument();
+  });
+
+  it("should display the amount available", () => {
+    expect(
+      screen.getByText(new RegExp(`${mockPaper.QteAtivo}`, "i")),
+    ).toBeInTheDocument();
+  });
+
+  it("should display the variation", () => {
+    expect(
+      screen.getByText(new RegExp(`${mockPaper.Variacao}`, "i")),
+    ).toBeInTheDocument();
   });
 });
