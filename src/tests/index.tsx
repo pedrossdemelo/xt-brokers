9bdfd3f76f63743843246e669727a938bfda1cf2
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 01:21:50 -0300

    test: add mocks

--- src/tests/index.tsx
+++ src/tests/index.tsx
@@ -1,23 +1,44 @@
 import { cleanup, render } from "@testing-library/react";
-import { UserProvider } from "context";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { afterEach } from "vitest";
+import { MockContext, UserProviderMock } from "./mocks";
 
 afterEach(() => {
   cleanup();
 });
 
-function Context({ children }: { children: React.ReactNode }) {
-  return (
-    <UserProvider>
-      <BrowserRouter>{children}</BrowserRouter>
-    </UserProvider>
-  );
-}
+const FakeContext = (mock?: MockContext, route?: string) =>
+  function Context({ children }: { children: React.ReactNode }) {
+    let paramValue, url;
+    if (route) {
+      [url, paramValue] = route.split("=");
+      route = url;
+      url = route.split(":")[0] + paramValue;
+    }
+    return (
+      <UserProviderMock mockContext={mock}>
+        <MemoryRouter initialEntries={[url ?? "/"]}>
+          <Routes>
+            <Route path={route ?? "/"} element={children} />
+          </Routes>
+        </MemoryRouter>
+      </UserProviderMock>
+    );
+  };
 
-const renderWithContext = (ui: React.ReactElement, options = {}) =>
-  render(ui, { wrapper: Context, ...options });
+const renderWithContext = (
+  ui: React.ReactElement,
+  options: Parameters<typeof render>[1] & {
+    mock?: MockContext;
+    route?: string;
+  } = {},
+) => {
+  const [mock, route] = [options.mock, options.route];
+  delete options.mock;
+  delete options.route;
+  return render(ui, { wrapper: FakeContext(mock, route), ...options });
+};
 
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
---
+++ src/tests/mocks/UserContextMock.tsx
@@ -0,0 +1,77 @@
+import { UserContext } from "context";
+import { useLocalStorage } from "hooks";
+import React, { useRef } from "react";
+import { Paper, papers } from "./mockPapers";
+
+type ExcludeValuesOfType<U, T> = {
+  [K in keyof T]: T[K] extends U ? never : T[K];
+};
+
+export type MockContext = ExcludeValuesOfType<Function, UserMockContextValue>;
+
+type Props = {
+  children: React.ReactNode;
+  mockContext?: MockContext;
+};
+
+type UserMockContextValue = {
+  userPapers: typeof papers;
+  setUserPapers: React.Dispatch<React.SetStateAction<typeof papers>>;
+  allPapers: typeof papers;
+  setAllPapers: React.Dispatch<React.SetStateAction<typeof papers>>;
+  user: string;
+  setUser: React.Dispatch<React.SetStateAction<string>>;
+  funds: number;
+  setFunds: React.Dispatch<React.SetStateAction<number>>;
+  portfolio: number;
+  loggedAt: Date;
+  logout: () => void;
+  hideMoney: boolean;
+  setHideMoney: React.Dispatch<React.SetStateAction<boolean>>;
+  mockContext?: MockContext;
+};
+
+export function UserProviderMock({ children, mockContext }: Props) {
+  const [userPapers, setUserPapers] = useLocalStorage(
+    "userPapers",
+    [] as Paper[],
+  );
+  const [allPapers, setAllPapers] = useLocalStorage("allPapers", papers);
+  const [user, setUser] = useLocalStorage("user", "test@gmail.com");
+  const [funds, setFunds] = useLocalStorage("funds", 10000);
+  const loggedAt = useRef(new Date());
+  const logout = () => {
+    setUser("");
+    setUserPapers([]);
+    setFunds(10000);
+    setAllPapers(papers);
+    loggedAt.current = new Date();
+  };
+  const portfolio = React.useMemo(
+    () =>
+      userPapers.reduce((acc, paper) => {
+        return acc + paper.Valor * paper.QteAtivo;
+      }, 0),
+    [userPapers],
+  );
+  const [hideMoney, setHideMoney] = useLocalStorage("hideMoney", false);
+
+  const value = {
+    userPapers,
+    setUserPapers,
+    allPapers,
+    setAllPapers,
+    user,
+    setUser,
+    funds,
+    setFunds,
+    portfolio,
+    loggedAt: loggedAt.current,
+    logout,
+    hideMoney,
+    setHideMoney,
+    ...mockContext,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
