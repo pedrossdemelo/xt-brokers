725f849ae3e63dec3ceb8e93271a3a4fc8fb2614
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 18:21:25 -0300

    test: UserPapers

--- src/tests/components/UserPapers.test.tsx
+++ src/tests/components/UserPapers.test.tsx
@@ -1,9 +1,54 @@
 import { UserPapers } from "components";
-import { render } from "tests";
+import { render, screen, within } from "tests";
+import { papers } from "tests/mocks";
 
-describe("UserPapers component", () => {
-  it.todo("implement tests");
-  it("should render", () => {
-    render(<UserPapers />);
+const mockPapers = papers
+  .slice(0, 5)
+  .map((paper) => ({ ...paper, QteAtivo: 10 }));
+
+const total = mockPapers.reduce(
+  (acc, paper) => acc + paper.Valor * paper.QteAtivo,
+  0,
+);
+
+describe("UserPapers", () => {
+  beforeEach(() => {
+    render(<UserPapers />, { mock: { userPapers: mockPapers } });
+  });
+
+  it("should display all papers in the portfolio", async () => {
+    for (const paper of mockPapers) {
+      const paperElement = document.getElementById(
+        `carousel-${paper.CodAtivo}`,
+      )!;
+
+      await within(paperElement).findByText(paper.CodAtivo);
+
+      await within(paperElement).findByText(paper.NomeAtivo);
+
+      await within(paperElement).findByText(new RegExp(`${paper.Valor}`, "i"));
+
+      await within(paperElement).findByText(
+        new RegExp((paper.Valor * paper.QteAtivo).toFixed(2), "i"),
+      );
+
+      await within(paperElement).findByText(
+        new RegExp(`${paper.Variacao}`, "i"),
+      );
+
+      await within(paperElement).findByText(
+        new RegExp(`${paper.QteAtivo} shares`, "i"),
+      );
+    }
+  });
+
+  it("should display 'Portfolio' at the top", () => {
+    expect(screen.getByText(/portfolio/i)).toBeInTheDocument();
+  });
+
+  it("should display the total portfolio value", () => {
+    expect(
+      screen.getByText(new RegExp(total.toFixed(2), "i")),
+    ).toBeInTheDocument();
   });
 });
