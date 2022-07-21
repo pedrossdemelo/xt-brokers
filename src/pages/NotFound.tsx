a0e3c0bcf7133ec9bb2ad389dd0fa13265a5cb41
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-21 03:35:39 -0300

    feat: style NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,4 @@
-import { ViewGridIcon } from "@heroicons/react/solid";
+import { QuestionMarkCircleIcon, ViewGridIcon } from "@heroicons/react/solid";
 import { Button } from "flowbite-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
@@ -19,8 +19,11 @@ export default function NotFound() {
   const { goBack } = useNotFound();
 
   return (
-    <div>
-      This page does not exist.
+    <div className="grow flex flex-col items-center relative justify-center">
+      <QuestionMarkCircleIcon className="h-64 absolute opacity-5 pointer-events-none top-0 translate-y-[135%]" />
+
+      <h1 className="text-xl text-medium mb-4">This page does not exist.</h1>
+
       <Button color="gray" onClick={goBack}>
         <ViewGridIcon className="h-6 mr-3" />
         Return to Dashboard
