689ad13d5e81124e04992d43a186d3917b31e6af
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-20 21:57:14 -0300

    test: useUserData.

--- src/tests/hooks/useUserData.test.ts
+++ src/tests/hooks/useUserData.test.ts
@@ -1,5 +1,29 @@
-import { describe, it } from "vitest";
+import { useUserData } from "hooks";
+import { renderHook } from "tests";
+import { papers } from "tests/mocks";
 
-describe("useUserData hook", () => {
-  it.todo("implement tests");
+const contextMock = {
+  user: "test@test.com",
+  userPapers: [],
+  allPapers: papers,
+  portfolio: 1000,
+  funds: 1000,
+  loggedAt: new Date(),
+  hideMoney: false,
+};
+
+describe("useUserData", () => {
+  it("should return the values inside of UserDataContext", () => {
+    const { result } = renderHook(() => useUserData(), {
+      mock: contextMock,
+    });
+
+    const context = result.current;
+
+    for (const [key, value] of Object.entries(context)) {
+      if (typeof value === "function") continue;
+      // @ts-ignore
+      expect(value).toStrictEqual(contextMock?.[key]);
+    }
+  });
 });
