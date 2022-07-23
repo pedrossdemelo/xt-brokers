cfa9935a5a71dcc105dc75245cc08011d8fdf0f2
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-22 22:53:48 -0300

    chore: update index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,7 @@
+export { default as fetchAllPapers } from "./fetchAllPapers";
+export { default as fetchFunds } from "./fetchFunds";
+export { default as fetchTransactions } from "./fetchTransactions";
+export { default as fetchUserPapers } from "./fetchUserPapers";
+export { default as postTransaction } from "./postTransaction";
 export { default as PrivateRoutes } from "./PrivateRoutes";
 export { default as supabase } from "./supabase";
