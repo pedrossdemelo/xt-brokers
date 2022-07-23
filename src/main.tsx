79154662a47438eba1a1236015168100f54c5c64
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-22 21:56:44 -0300

    fix: put browser router before context

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <UserProvider>
         <App />
-      </BrowserRouter>
-    </UserProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
