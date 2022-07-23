850c76ab6fefcad3fad3fc07cd79dd91ff3f9628
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 13:06:10 -0300

    test: Transactions page

---
+++ src/tests/components/Transactions.test.tsx
@@ -0,0 +1,34 @@
+import { Transactions } from "pages";
+import { render, screen, within } from "tests";
+import { transactions } from "tests/mocks";
+
+describe("Transactions", () => {
+  beforeEach(() => {
+    render(<Transactions />);
+  });
+
+  it("should display all user transactions", async () => {
+    for (const transaction of transactions) {
+      const date = new Date(transaction.data);
+
+      const dateString = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+      const factor = transaction.venda ? 1 : -1;
+
+      const transactionElement = document.getElementById(
+        `transactions-${transaction.id}`,
+      )!;
+
+      await within(transactionElement).findByText(transaction.codAtivo);
+      await within(transactionElement).findByText(dateString);
+      await within(transactionElement).findByText(
+        new RegExp(`${(transaction.valor * factor).toFixed(2)}`, "i"),
+      );
+      await within(transactionElement).findByText(transaction.qtdeAtivo);
+    }
+  });
+
+  it("should display 'transactions' at the top", () => {
+    expect(screen.getByText(/transactions/i)).toBeInTheDocument();
+  });
+});
