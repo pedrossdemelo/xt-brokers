e1a6e604eeba0fdc01cd128cd84a676964468afc
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 00:14:53 -0300

    refactor: postTransactions

--- src/utils/postTransaction.ts
+++ src/utils/postTransaction.ts
@@ -1,14 +1,15 @@
 import supabase from "./supabase";
 
 export default function postTransaction(
+  type: "sell" | "buy",
+  amount: number,
   ticker: string,
-  { amount, sell }: { amount: number; sell: boolean },
 ) {
   return supabase.from("transacoes").insert([
     {
       codAtivo: ticker,
       qtdeAtivo: amount,
-      venda: sell,
+      venda: type === "sell",
     },
   ]);
 }
