68f92e3eba34b7d3f382d7de1b718c3421459388
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 15:18:12 -0300

    feat: add transactions even with realtime disabled

--- src/hooks/usePaperTransaction.ts
+++ src/hooks/usePaperTransaction.ts
@@ -1,23 +1,45 @@
 import produce from "immer";
 import React from "react";
 import { Paper } from "tests/mocks";
-import { postTransaction, supabase } from "utils";
+import { postTransaction } from "utils";
 import useUserData from "./useUserData";
 
 export default function usePaperTransaction(paper: Paper) {
-  const { setUserPapers, setAllPapers, funds, setFunds } = useUserData();
+  const { setUserPapers, setAllPapers, funds, setFunds, setTransactions } =
+    useUserData();
   const [loading, setLoading] = React.useState(false);
 
   const sellPaper = React.useCallback(
     async (amount: number) => {
-      console.log(supabase.getSubscriptions());
       setLoading(true);
-      const { error } = await postTransaction("sell", amount, paper.codAtivo);
+
+      const { data, error } = await postTransaction(
+        "sell",
+        amount,
+        paper.codAtivo,
+      );
 
       if (error) {
         // TODO: inform user with error toast
         console.error(error);
         return setLoading(false);
+      }
+
+      if (data) {
+        delete data[0].codCliente;
+        setTransactions((old) => [data[0], ...old]);
+      } else {
+        setTransactions((old) => [
+          {
+            codAtivo: paper.codAtivo,
+            data: new Date().toISOString(),
+            valor: paper.valor * amount,
+            venda: true,
+            qtdeAtivo: amount,
+            id: old.at(-1)?.id ?? 0 + 1,
+          },
+          ...old,
+        ]);
       }
 
       setUserPapers(
@@ -56,12 +78,34 @@ export default function usePaperTransaction(paper: Paper) {
   const buyPaper = React.useCallback(
     async (amount: number) => {
       setLoading(true);
-      const { error } = await postTransaction("buy", amount, paper.codAtivo);
+
+      const { data, error } = await postTransaction(
+        "buy",
+        amount,
+        paper.codAtivo,
+      );
 
       if (error) {
         // TODO: inform user with error toast
         console.error(error);
         return setLoading(false);
+      }
+
+      if (data) {
+        delete data[0].codCliente;
+        setTransactions((old) => [data[0], ...old]);
+      } else {
+        setTransactions((old) => [
+          {
+            codAtivo: paper.codAtivo,
+            data: new Date().toISOString(),
+            valor: paper.valor * amount,
+            venda: true,
+            qtdeAtivo: amount,
+            id: old.at(-1)?.id ?? 0 + 1,
+          },
+          ...old,
+        ]);
       }
 
       setAllPapers(
