d7ce80dc1138fbc7829abd1f1898547ee780d431
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 13:07:40 -0300

    feat: add Transaction component

---
+++ src/components/Transaction.tsx
@@ -0,0 +1,25 @@
+import { Transaction as TTransaction } from "tests/mocks";
+
+type Props = {
+  data: TTransaction;
+};
+
+export default function Transaction({ data }: Props) {
+  const date = new Date(data.data);
+
+  const dateString = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+  const factor = data.venda ? 1 : -1;
+
+  return (
+    <div id={`transaction-${data.id}`}>
+      <div>{data.codAtivo} </div>
+
+      <div>x {data.qtdeAtivo} </div>
+
+      <div>{data.valor * factor} </div>
+
+      <div>{dateString} </div>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,4 +6,5 @@ export { default as Header } from "./Header";
 export { default as Loader } from "./Loader";
 export { default as LoginInput } from "./LoginInput";
 export { default as Paper } from "./Paper";
+export { default as Transaction } from "./Transaction";
 export { default as UserPapers } from "./UserPapers";
