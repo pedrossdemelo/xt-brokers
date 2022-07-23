e9e78f6f6c7bde953bb2cae6f94056bb26e257b3
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 01:02:28 -0300

    fix: remove from state any papers with no availability

--- src/utils/fetchAllPapers.ts
+++ src/utils/fetchAllPapers.ts
@@ -1,6 +1,14 @@
 import { Paper } from "tests/mocks";
 import { supabase } from "utils";
 
-export default function fetchAllPapers() {
-  return supabase.from<Paper>("investimentos").select("*");
+export default async function fetchAllPapers() {
+  const { data, error } = await supabase
+    .from<Paper>("investimentos")
+    .select("*");
+  return {
+    error,
+    data: (data?.filter((paper) => paper.qtdeAtivo > 0) ?? null) as
+      | Paper[]
+      | null,
+  };
 }
