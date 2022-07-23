b72976a1d69a75925b5a58b0157b6ffb5bcdafe2
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 14:15:51 -0300

    test: mock incrementFunds

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,5 +2,8 @@ import "@testing-library/jest-dom";
 import { vi } from "vitest";
 
 vi.mock("utils", async () => {
-  return { ...vi.importMock("utils") };
+  return {
+    ...vi.importMock("utils"),
+    incrementFunds: async () => ({ data: null, error: null }),
+  };
 });
