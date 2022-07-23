89fe0651e7908493e5f3f5e31144db127eec7105
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 00:38:07 -0300

    chore: update index

--- src/tests/mocks/index.ts
+++ src/tests/mocks/index.ts
@@ -1,4 +1,7 @@
 export { papers } from "./mockPapers";
 export type { Paper } from "./mockPapers";
+export { transactions } from "./mockTransactions";
+export type { Transaction } from "./mockTransactions";
+export { default as useMockPaperTransaction } from "./useMockPaperTransaction";
 export { UserProviderMock } from "./UserContextMock";
 export type { MockContext } from "./UserContextMock";
