42787bb26c4dc6203e54734c6cec13c31a4156e5
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-23 21:52:25 -0300

    style: Home sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,17 @@
+import { ViewGridIcon } from "@heroicons/react/solid";
 import { LoginInput } from "components";
 
 export default function Home() {
   return (
     <div className="flex grow">
-      <div className="hidden md:block w-72 bg-slate-900 text-white py-8 px-12">
-        <h1 className="text-4xl">XT Brokers</h1>
+      <div className="hidden md:flex w-72 relative items-center justify-between bg-slate-900 flex-col text-white py-8 px-12">
+        <div>
+          <h1 className="text-4xl font-medium">XT Brokers</h1>
 
-        <p className="w-full text-sm">Trading simulator</p>
+          <p className="w-full text-sm text-right">Trading Simulator</p>
+        </div>
+
+        <ViewGridIcon className="h-56 opacity-5" />
       </div>
 
       <div className="flex flex-col grow items-center justify-center p-4 md:p-10">
