881b628e911ab70a56b429a6f490311abee89a6b
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-07-24 01:17:20 -0300

    docs: add timeline item

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -2,10 +2,34 @@ import { Timeline } from "flowbite-react";
 
 export default function Announcements() {
   return (
-    <div className="p-4">
+    <div className="p-4 max-w-md md:px-6">
       <h3 className="font-medium text-xl mb-4">Announcements</h3>
 
       <Timeline>
+        <Timeline.Item>
+          <Timeline.Point />
+
+          <Timeline.Content>
+            <Timeline.Time>23 July 2022</Timeline.Time>
+
+            <Timeline.Title>Realtime updates are now live!</Timeline.Title>
+
+            <Timeline.Body>
+              <span className="text-sm">
+                Supabase fixed a bug that caused the realtime subscriptions
+                triggers to not work. The<> </>
+                <a
+                  className="text-blue-700 underline"
+                  href="https://github.com/supabase/realtime/issues/268#issuecomment-1193184394"
+                >
+                  issue
+                </a>{" "}
+                was reported and fixed within 24 hours. Props to them!
+              </span>
+            </Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
         <Timeline.Item>
           <Timeline.Point />
 
