6645da26c701ebf066bbf542e18619e328fd0d4a
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2026-05-03 04:19:36 +0200

    feat: apply dark class from URL theme param

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import "styles/globals.css";
 import App from "./App";
 
+const urlTheme = new URLSearchParams(window.location.search).get("theme");
+if (urlTheme === "dark") document.documentElement.classList.add("dark");
+else if (urlTheme === "light")
+  document.documentElement.classList.remove("dark");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
