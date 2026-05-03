56f35a0c0361332fda5fa2fa67e03188de2f2b8d
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2026-05-03 04:19:00 +0200

    refactor: use VITE_SUPABASE_PUBLISHABLE_KEY in client

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export default createClient(supabaseUrl, supabaseAnonKey);
+export default createClient(supabaseUrl, supabasePublishableKey);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string;
+}
